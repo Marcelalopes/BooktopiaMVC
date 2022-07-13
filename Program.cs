5c611ed6533f13677f0478ac94575059185ad4ea
Marcela <marcelalopes497@gmail.com>
2022-07-13 12:49:40 -0300

    concertando erro de preco

--- Program.cs
+++ Program.cs
@@ -17,7 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseRequestLocalization("pt-br");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
